0957a04a895146b98857a7baee72fd4f012a570b
jrsmoura98 <129577935+jrsmoura98@users.noreply.github.com>
2025-11-13 01:01:04 -0300

    Update instagram.js

--- api/instagram.js
+++ api/instagram.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import qs from "qs";
 
 export default async function handler(req, res) {
   try {
@@ -8,7 +9,8 @@ export default async function handler(req, res) {
       return res.status(400).json({ error: "username obrigatório" });
     }
 
-    const url = "https://instagram-scraper-stable-api.p.rapidapi.com/ig_get_fb_profile.php";
+    const url =
+      "https://instagram-scraper-stable-api.p.rapidapi.com/ig_get_fb_profile.php";
 
     const headers = {
       "x-rapidapi-key": process.env.RAPIDAPI_KEY,
@@ -16,19 +18,20 @@ export default async function handler(req, res) {
       "Content-Type": "application/x-www-form-urlencoded",
     };
 
-    const body = new URLSearchParams({
+    const body = qs.stringify({
       username_or_url: username,
       data: "basic",
     });
 
-    const response = await axios.post(url, body.toString(), { headers });
+    const response = await axios.post(url, body, { headers });
 
     return res.status(200).json(response.data);
-
   } catch (error) {
+    console.log("ERRO RapidAPI >>>", error?.response?.data || error.message);
+
     return res.status(500).json({
       error: "erro ao consultar instagram",
-      detail: error?.response?.data ?? error.message,
+      detail: error?.response?.data || error.message,
     });
   }
 }
